eb25f3323585dea2c053e83a035f5e98d8bb9936
Shiroan <shiroan@qq.com>
2021-04-16 19:33:37 +0800

    Add 1 exam.

---
+++ exercises/C程序基础结构/exam.c
@@ -0,0 +1,15 @@
+// 恭喜你，终于跨进C语言的大门了！
+// 让我们看看，你是否已经准备好了吧！
+// 请你试着改正以下程序的错误：
+
+// I AM NOT DONE
+
+#include <stdio.h>
+
+int mian()
+{
+    int a, b；
+    scanf("%d %d", a, b);
+    printf("%d %d", a, b);
+    retrun 0;
+}
